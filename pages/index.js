b6ad73c687ae3d29991f3ebb24eefe1d2ad0bebc
Bambam <akinbams11@gmail.com>
2025-08-07 07:45:57 +0100

    new

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 'use client';
-import Hero from '@/components/Hero';
-import Projects from '@/components/Projects';
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
 
 export default function Home() {
   return (
